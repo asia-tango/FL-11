8221d79fd34ad40c3af5950cdf70f89b78c97852
Oleksandra Kaplun <kaplunoleksandra@gmail.com>
2019-07-04 17:36:06 +0300

    Two tasks without corrections

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,28 @@
-// Your code goes here
+const two = 2;
+
+let a1 = prompt('Enter the coordinate point a1', ''); 
+let a2 = prompt('Enter the coordinate point a2', '');  
+let b1 = prompt('Enter the coordinate point b1', '');  
+let b2 = prompt('Enter the coordinate point b2', '');  
+let c1 = prompt('Enter the coordinate point c1', '');  
+let c2 = prompt('Enter the coordinate point c2', ''); 
+
+a1 = parseFloat(a1);
+a2 = parseFloat(a2);
+b1 = parseFloat(b1);
+b2 = parseFloat(b2);
+c1 = parseFloat(c1);
+c2 = parseFloat(c2);
+
+if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
+	console.log('Error! Please enter correct coordinates.');
+} else { 
+	let cx = (a1 + b1) / two;
+	let cy = (a2 + b2) / two;
+
+	if (cx===c1 && cy===c2) { 
+		console.log('true');
+	} else { 
+		console.log('false');
+	}
+}
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,23 @@
-// Your code goes here
+const two = 2;
+
+let a = prompt('Enter the coordinate point a', ''); 
+let b = prompt('Enter the coordinate point b', ''); 
+let c = prompt('Enter the coordinate point c', '');  
+
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+
+let x = a + b;
+let y = a + c;
+let z = b + c;
+
+if (isNaN(a) || isNaN(b) || isNaN(c) || a<=0 || b<=0 || c<=0 || x<=c || y<=b || z<=a) {
+	console.log('Triangle doesn’t exist.');
+} else if (a===b && a===c) {
+	console.log('Equivalent triangle');
+} else if (a===b || b===c || a===c) {
+	console.log('Isosceles triangle');
+} else {
+	console.log('Normal triangle');
+}
