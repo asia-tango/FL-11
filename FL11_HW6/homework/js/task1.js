5ab10e9f0a5c649ca6c914dc1c60b15fbeec0cd7
Oleksandra Kaplun <kaplunoleksandra@gmail.com>
2019-07-06 16:10:18 +0300

    Fixed - all points in one coordinate

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,11 +1,11 @@
 const two = 2;
 
-let a1 = prompt('Enter the coordinate point a1', ''); 
-let a2 = prompt('Enter the coordinate point a2', '');  
-let b1 = prompt('Enter the coordinate point b1', '');  
-let b2 = prompt('Enter the coordinate point b2', '');  
-let c1 = prompt('Enter the coordinate point c1', '');  
-let c2 = prompt('Enter the coordinate point c2', ''); 
+let a1 = prompt( 'Enter the coordinate point a1', '' ); 
+let a2 = prompt( 'Enter the coordinate point a2', '' );  
+let b1 = prompt( 'Enter the coordinate point b1', '' );  
+let b2 = prompt( 'Enter the coordinate point b2', '' );  
+let c1 = prompt( 'Enter the coordinate point c1', '' );  
+let c2 = prompt( 'Enter the coordinate point c2', '' ); 
 
 a1 = parseFloat(a1);
 a2 = parseFloat(a2);
@@ -16,11 +16,13 @@ c2 = parseFloat(c2);
 
 if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
 	console.log('Error! Please enter correct coordinates.');
+} else if ( a1===b1 || a1===c1 || a2===b2 || a2===c2 ) {
+	console.log('false');
 } else { 
-	let cx = (a1 + b1) / two;
-	let cy = (a2 + b2) / two;
+	let x = ( a1 + b1 ) / two;
+	let y = ( a2 + b2 ) / two;
 
-	if (cx===c1 && cy===c2) { 
+	if ( x===c1 && y===c2 ) { 
 		console.log('true');
 	} else { 
 		console.log('false');
