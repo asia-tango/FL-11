9fd0d0eb6f1b81063df9ccbd74ae2e6785a7179e
Oleksandra Kaplun <kaplunoleksandra@gmail.com>
2019-07-07 09:01:15 +0300

    Fixed - design of code

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -8,11 +8,11 @@ let c1 = parseFloat(prompt( 'Enter the coordinate point c1', '' ));
 let c2 = parseFloat(prompt( 'Enter the coordinate point c2', '' )); 
 
 if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
-	console.log('Error! Please enter correct coordinates.');
+	console.log( 'Error! Please enter correct coordinates.' );
 } else if ( a1===b1 || a1===c1 || a2===b2 || a2===c2 ) {
 	console.log( false );
 } else { 
 	let x = ( a1 + b1 ) / two;
 	let y = ( a2 + b2 ) / two;
-	console.log(x===c1 && y===c2);
+	console.log( x===c1 && y===c2 );
 }
