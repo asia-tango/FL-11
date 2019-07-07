87b8e97eb42e8913f7e4ed8b2d7251d8d317e52e
Oleksandra Kaplun <kaplunoleksandra@gmail.com>
2019-07-07 09:03:42 +0300

    Fixed - design of code - spaces

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,13 +1,13 @@
 const two = 2;
 
-let a1 = parseFloat( prompt( 'Enter the coordinate point a1', '' )); 
-let a2 = parseFloat( prompt( 'Enter the coordinate point a2', '' ));  
-let b1 = parseFloat( prompt( 'Enter the coordinate point b1', '' ));  
-let b2 = parseFloat( prompt( 'Enter the coordinate point b2', '' ));  
-let c1 = parseFloat(prompt( 'Enter the coordinate point c1', '' ));  
-let c2 = parseFloat(prompt( 'Enter the coordinate point c2', '' )); 
+let a1 = parseFloat( prompt( 'Enter the coordinate point a1', '' ) ); 
+let a2 = parseFloat( prompt( 'Enter the coordinate point a2', '' ) );  
+let b1 = parseFloat( prompt( 'Enter the coordinate point b1', '' ) );  
+let b2 = parseFloat( prompt( 'Enter the coordinate point b2', '' ) );  
+let c1 = parseFloat( prompt( 'Enter the coordinate point c1', '' ) );  
+let c2 = parseFloat( prompt( 'Enter the coordinate point c2', '' ) ); 
 
-if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2)) {
+if ( isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2) || isNaN(c1) || isNaN(c2) ) {
 	console.log( 'Error! Please enter correct coordinates.' );
 } else if ( a1===b1 || a1===c1 || a2===b2 || a2===c2 ) {
 	console.log( false );
