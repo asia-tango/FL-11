88411d839953520bd3c4e6afecd26700512b5df3
Oleksandra Kaplun <kaplunoleksandra@gmail.com>
2019-07-10 00:02:06 +0300

    game3

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,6 +1,6 @@
 const ZERO = 0;
-let PRIZE_INCREASE = 2;
-let RANDOM_INCREASE = 4;
+const PRIZE_INCREASE = 2;
+const RANDOM_INCREASE = 4;
 
 let randomNum = 2;
 let userNum;
@@ -14,52 +14,52 @@ let guess;
 
 let playGame = confirm('Do you want to play a game?');
 if (!playGame) {
-    alert('You did not become a billionaire, but can.');
+ alert('You did not become a billionaire, but can.');
 } else {
-    do {
-        alert(randomNum);
-        let attempt = maxAttempts;
-        /*let randomNumber = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
-        alert(randomNumber);*/
-        do {
-            guess = prompt(Choose a roulette pocket from ${minRange} to ${maxRange} +
-                '\nAttempts left: ' +${attempt} +
-                '\nTotal prize: ' + ${prizeTotal} +
-                '\nPossible prize on the current attempt: ' + ${prize}, '');
-
-        } while (isNaN(guess) || guess === '' || guess === null
-        || guess > maxRange || guess < minRange);
+ do {
+    let attempt = maxAttempts;
+    let randomNumber = Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
+    alert(randomNumber);
+        
+         do {
+                guess = prompt(`Choose a roulette pocket from ${minRange} 
+                to ${maxRange}` +
+                                '\nAttempts left: ' +${attempt} +
+                                '\nTotal prize: ' + ${prizeTotal} +
+                                '\nPossible prize on the current attempt: ' + ${prize}, '');
+            
+            } while (isNaN(guess) || guess === '' || guess === null 
+                || guess > maxRange || guess < minRange);
         if (guess === randomNum) {
-            prizeTotal = prizeTotal + prize;
-            continueGame = confirm(Congratulation, you won! Your prize is: ${prizeTotal}$. Do you want to continue?);
-
+         prizeTotal = prizeTotal + prize;
+            continueGame = confirm(`Congratulation, you won! Your prize is: ${prizeTotal}$. 
+                Do you want to continue?`);
             if (continueGame) {
-                attempt = maxAttempts;
-                maxRange = maxRange + RANDOM_INCREASE;
-                prize = prize * PRIZE_INCREASE;
+             attempt = maxAttempts;
+          maxRange = maxRange + RANDOM_INCREASE;
+             prize = prize * PRIZE_INCREASE;
 
             } else {
-                prizeTotal = ZERO;
+             prizeTotal = ZERO;
             }
-        } else {
-            do{
-                attempt--;
-                prize = prize / PRIZE_INCREASE;
-                confirm('Thank you for your participation. Your prize is: ' +
-                    ${prizeTotal}$.  + '\nDo you want to play again?');
-                if (continueGame) {
-                    prizeTotal = prizeTotal + prize;
-                    attempt = maxAttempts;
-                }
-            } while (attempt > ZERO);
-        }
-
-        if (attempt === ZERO) {
+        } else { 
+         do{
+         attempt--;
+            prize = prize / PRIZE_INCREASE;
             confirm('Thank you for your participation. Your prize is: ' +
-                ${prizeTotal}$.  + '\nDo you want to play again?');
+            ${prizeTotal}$.  + '\nDo you want to play again?');
+            if (continueGame) {
+             prizeTotal = prizeTotal + prize;
+             attempt = maxAttempts;
+            }
+        } while (attempt > ZERO);
+}  
+        if (attempt === ZERO) {
+         confirm('Thank you for your participation. Your prize is: ' +
+            ${prizeTotal}$.  + '\nDo you want to play again?');
             prizeTotal = prize;
         } else {
-            continueGame = false;
+         continueGame = false;
         }
-    } while(continueGame);
-}
+  } while (continueGame);
+ }
